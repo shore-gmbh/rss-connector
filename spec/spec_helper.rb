227c75cc0464294ac1f4a2586726330d5bc23f23
Dylan Johnston <qdylanj@gmail.com>
2016-01-26 18:23:14 +0100

    Fix rubocop warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rss'
+require 'securerandom'
+
+Time.zone = 'Europe/Berlin'
+
+RSS.configure do |config|
+  config.base_uri = 'testhost'
+  config.secret = 'secret'
+end
+
+RSS.load!
